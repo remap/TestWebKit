9665bb121980323444f19b5018ad4ca5ec7eb397
peetonn <gpeetonn@gmail.com>
2016-11-09 15:47:23 -0800

    Initial Commit

---
+++ TestWebKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWebKit
+//
+//  Created by Peter Gusev on 11/9/16.
+//  Copyright © 2016 Peter Gusev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestWebKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWebKit
+//
+//  Created by Peter Gusev on 11/9/16.
+//  Copyright © 2016 Peter Gusev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
